c80b1e664ac0b438c550ba0a25579d7a9d8e089a
Vlad Istodorescu <166702009+vladistodorescu@users.noreply.github.com>
2026-01-12 11:28:22 +0200

    Fix file handling and improve argument processing

--- Overwriter-And-Reader-From-A-Given-File/task1.c
+++ Overwriter-And-Reader-From-A-Given-File/task1.c
@@ -2,60 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
     if (argc < 3){
+        printf("Not enough arguments! Format: %s <filename> <content_1> <content_2> ...", argv[0]);
         exit(1);
     }
-    
+
     char fileName[256];
-    char content[256];
-    content[0] = '\0';
+    char content[256] = "";
 
     for (int i = 0; i < argc; i++){
         if (i == 1){
-            strncpy(fileName, argv[i], sizeof(fileName) - 1);
-            fileName[sizeof(fileName) - 1] = '\0';   
+            snprintf(fileName, sizeof(fileName), "%s", argv[i]);
         }
-        
+
         if (i > 1){
             strcat(content, argv[i]);
         }
     }
-        
+
     FILE *file = fopen(fileName, "w+");
-    if (f == NULL){
+    if (file == NULL){
         perror("Error at fopen");
         exit(2);
     }
-    
+
     pid_t pid = fork();
-    
-    // error when forking
+
+    // Error when forking
     if (pid < 0){
         perror("fork");
         exit(3);
     }
-    // child process
+    // Child process writes to file
     else if (pid == 0){
         fputs(content, file);
         fclose(file);
         exit(0);
     }
+    // Parent for file to be written too, then reads it
     else {
-        wait(NULL);  
+        wait(NULL);
         rewind(file);
-        
+
         int ch;
         while ((ch = fgetc(file)) != EOF) {
-            putchar(ch);
+            printf("%c", ch);
         }
-        putchar('\n');
+        printf("\n");
         fclose(file);
-    } 
-        
+    }
+
     return 0;
 }
+
